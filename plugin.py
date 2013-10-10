672179a1c53d0ab3f5b6ba37e639eab2873bebcf
Finn Herzfeld <thefinn93@thefinn93.com>
2013-10-10 16:52:53 -0700

    Fixed typo that was causing it to not actually work

--- plugin.py
+++ plugin.py
@@ -88,7 +88,7 @@ class VersionCheck(callbacks.Plugin):
                     hostmask = "%s!%s@%s" % (msg.nick, msg.user, msg.host)
                     sendNotice = True
                     if hostmask not in self.recentnotices:
-                        self.recentnotices[hostmaks] = datetime.now()
+                        self.recentnotices[hostmask] = datetime.now()
                     else:
                         if datetime.now() - self.recentnotices[hostmask] < timedelta(hours=6):
                             sendNotice = False
